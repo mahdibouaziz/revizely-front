4585867550dc514d34d3a261d9f57c735c8284a2
hanabenasker <hanabenasker@insat.u-carthage.tn>
2021-12-03 15:34:19 +0100

    user profile added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { CoursesListComponent } from './courses-list/courses-list.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ProfileComponent } from './profile/profile.component';
 import { UsersListComponent } from './users-list/users-list.component';
 
 const routes: Routes = [
@@ -11,7 +12,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'courses', component: CoursesListComponent },
   { path: 'users', component: UsersListComponent },
-
+  {path:'profile/:name' , component:ProfileComponent},
   { path: '', component: HomePageComponent },
 ]; // sets up routes constant where you define your routes
 
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  data=""
+  constructor( private router: ActivatedRoute) { }
 
   ngOnInit(): void {
+    console.log(this.router.snapshot.params)
+    this.data = this.router.snapshot.params.name
   }
 
 }
